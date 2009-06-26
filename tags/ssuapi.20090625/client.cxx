16d04382216bb4c922b7f8932d555be7dcb11336
akabigbro <akabigbro@a33c14ec-61ba-11de-a6e1-e33d94ba4d19>
2009-06-26 21:55:29 +0000

    added lazy initialization with a setter for the socket

--- tags/ssuapi.20090625/client.cxx
+++ tags/ssuapi.20090625/client.cxx
@@ -2,7 +2,7 @@
 
 void Client::Execute(void)
 {
-    while (!Terminated)
+    while (NULL != socket && !Terminated)
     {
         char buffer[32] = {0};
         try
@@ -25,6 +25,11 @@ void Client::Execute(void)
     }
 }
 
+Client::Client(void)
+    : socket(NULL), managed(false)
+{
+}
+
 Client::Client(Socket * socket, bool managed)
     : socket(socket), managed(managed)
 {
@@ -33,4 +38,14 @@ Client::Client(Socket * socket, bool managed)
 Client::~Client(void)
 {
     delete socket;
+}
+
+void Client::setSocket(Socket * socket)
+{
+    this->socket = socket;
+}
+
+void Client::setManaged(bool managed)
+{
+    this->managed = managed;
 }
